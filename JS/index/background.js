f96cbd4c4e3e56706c1a446057720376bac0ac2a
OgGhostJelly <johnnycambodia09@gmail.com>
2022-06-22 01:01:43 +0700

    extra auto gen update

--- JS/index/background.js
+++
@@ -1,6 +0,0 @@
-fetch('/IMG/index/bg/bg.json')
-    .then(response => response.json())
-    .then(bg_count => {
-        let url = 'url(/IMG/index/bg/bg' + Math.floor(Math.random() * bg_count) + '.png)'
-        document.body.style.backgroundImage = url
-    })
